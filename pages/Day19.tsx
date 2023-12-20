98ff4bab225d6bbfcb498c5f181a39c0971f04ce
Mike Wright <thespoonftw@gmail.com>
2023-12-20 16:55:38 +0000

    day 19

--- pages/Day19.tsx
+++ pages/Day19.tsx
@@ -5,13 +5,17 @@ import Solver, { WorkingBox } from '../components/Solver';
 export default function Render() {
 
   const part1 = (input: string[]): number => {
-    const sorter = new MachinePartSorter(input);
-    console.log(sorter);
-    return sorter.machineParts.filter(m => m.isAccepted).reduce((acc, m) => acc + m.sumValue, 0);
+    const emptyRowIndex = input.indexOf("");
+    const sorter = new MachinePartSorter(input.slice(0, emptyRowIndex));
+    const parts = input.slice(emptyRowIndex + 1, input.length).map(l => createSimpleMachinePart(l));
+    return sorter.sortAndSum(parts);
   }
 
   const part2 = (input: string[]): number => {
-    return 0;
+    const emptyRowIndex = input.indexOf("");
+    const sorter = new MachinePartSorter(input.slice(0, emptyRowIndex));
+    const parts = [createAdvancedMachinePart()]
+    return sorter.sortAndSum(parts);
   }
 
 
@@ -22,51 +26,73 @@ export default function Render() {
   );
 }
 
+const COMBINATION_LIMIT: number = 4000;
+
 class MachinePartSorter {
 
   workflows: Map<string, Workflow> = new Map<string, Workflow>();
-  machineParts: MachinePart[];
+  accepted: MachinePart[] = [];
+  rejected: MachinePart[] = [];
 
   constructor(input: string[]) {
-
-    const i = input.indexOf("");
-    const workflowsArr = input.slice(0, i).map(l => new Workflow(l));
-    this.machineParts = input.slice(i + 1, input.length).map(l => new MachinePart(l))
-
+    const workflowsArr = input.map(l => new Workflow(l));
     workflowsArr.forEach(w => this.workflows.set(w.label, w));
-
-    for (const machinePart of this.machineParts) {
-      let workflow = this.workflows.get("in")!;
-      let ruleIndex = 0;
-
-      while (true) {
-
-        const rule = workflow.rules[ruleIndex];
-        console.log(rule);
-
-        let destination: string | null = rule.tryGetDestination(machinePart);
-        if (destination === "A") {
-          machinePart.isAccepted = true;
-          break;
-        } else if (destination === "R") {
-          machinePart.isAccepted = false;
-          break;
-        } else if (destination !== null) {
-          workflow = this.workflows.get(destination)!;
-          ruleIndex = 0;
-        } else {
-          ruleIndex++;
-        }
-      }
-    }
-
-      
-
-
-
   }
 
+  sortAndSum(input: MachinePart[]) : number {
+    input.forEach(p => this.sortRecursive(p, "in", 0));
 
+    let sum = 0;
+    for (const part of this.accepted) {
+      sum += part.getRange("x").getWidth() * part.getRange("m").getWidth() * part.getRange("a").getWidth() * part.getRange("s").getWidth();
+    }
+    return sum;
+  }
+
+  sortRecursive(part: MachinePart, destination: string, ruleIndex: number) {
+
+    if (destination === "A") {
+      this.accepted.push(part);
+      return;
+
+    } else if (destination === "R") {
+      this.rejected.push(part);
+      return;
+    }
+
+    const workflow = this.workflows.get(destination);
+    const rule = workflow!.rules[ruleIndex];
+    const [part1, part2] = rule.splitPart(part);
+
+    if (part1) {
+      this.sortRecursive(part1, rule.destination, 0);
+    }
+    if (part2) {
+      this.sortRecursive(part2, destination, ruleIndex + 1);
+    }
+
+  }
+}
+
+function createSimpleMachinePart(line: string) : MachinePart {
+  let map = new Map<string, Range>();
+  const split = line.slice(1, line.length - 1).split(",");
+    split.forEach(s => {
+      const [key, valueStr] = s.split("=");
+      const value = parseInt(valueStr)
+      const range = new Range(value, value);
+      map.set(key, range);
+    });
+  return new MachinePart(map);
+}
+
+function createAdvancedMachinePart() : MachinePart {
+  let map = new Map<string, Range>();
+  map.set("x", new Range(1, COMBINATION_LIMIT));
+  map.set("m", new Range(1, COMBINATION_LIMIT));
+  map.set("a", new Range(1, COMBINATION_LIMIT));
+  map.set("s", new Range(1, COMBINATION_LIMIT));
+  return new MachinePart(map);
 }
 
 class Workflow {
@@ -99,8 +125,8 @@ class Rule {
     this.destination = input;
   }
 
-  tryGetDestination(machinePart: MachinePart) : string | null {
-    return this.destination;
+  splitPart(machinePart: MachinePart) : [toSend: MachinePart | null, remainder: MachinePart | null] {
+    return [machinePart, null]
   }
 
 }
@@ -120,12 +146,15 @@ class ComparisonRule extends Rule {
     this.value = parseInt(split1[0].slice(2));
   }
 
-  tryGetDestination(machinePart: MachinePart) : string | null {
-    const machineValue = machinePart.attributeMap.get(this.attribute);
+  splitPart(machinePart: MachinePart) : [toSend: MachinePart | null, remainder: MachinePart | null] {
     if (this.type === ComparisonType.Greater) {
-      return machineValue! > this.value ? this.destination : null;
+      const toSend = machinePart.cloneAndEditMin(this.attribute, this.value + 1);
+      const remainder = machinePart.cloneAndEditMax(this.attribute, this.value);
+      return [toSend, remainder];
     } else {
-      return machineValue! < this.value ? this.destination : null;
+      const toSend = machinePart.cloneAndEditMax(this.attribute, this.value - 1);
+      const remainder = machinePart.cloneAndEditMin(this.attribute, this.value);
+      return [toSend, remainder];
     }
   }
 
@@ -133,21 +162,65 @@ class ComparisonRule extends Rule {
 
 class MachinePart {
 
-  str: string;
-  attributeMap: Map<string, number> = new Map<string, number>();
-  isAccepted: boolean | null = null;
-  sumValue: number = 0;
+  attributeMap: Map<string, Range> = new Map<string, Range>();
 
-  constructor(input: string) {
-    this.str = input;
-    const split = input.slice(1, input.length - 1).split(",");
-    split.forEach(s => {
-      const [key, value] = s.split("=");
-      this.attributeMap.set(key, parseInt(value));
-      this.sumValue += parseInt(value);
-    });
+  constructor(map: Map<string, Range>) {
+    this.attributeMap = map;
   }
 
+  clone() : MachinePart {
+    const clonedMap = new Map<string, Range>();
+    for (const [key, value] of this.attributeMap.entries()) {
+      clonedMap.set(key, value.clone());
+    }
+    return new MachinePart(clonedMap);
+  }
+
+  getSumOfAttributes(): number {
+    let sum = 0;
+    return [...this.attributeMap.values()].reduce((acc, r) => acc + r.min, 0);
+  }  
+
+  cloneAndEditMin(attr: string, min: number) : MachinePart | null {
+    const range = this.attributeMap.get(attr)!;
+    if (min > range.max) { return null; }
+    const clone = this.clone();
+    if (min < range.min) { return clone; }
+    clone.attributeMap.get(attr)!.min = min;
+    return clone;
+  }
+
+  cloneAndEditMax(attr: string, max: number) : MachinePart | null {
+    const range = this.attributeMap.get(attr)!;
+    if (max < range.min) { return null; }
+    const clone = this.clone();
+    if (max > range.max) { return clone; }
+    clone.attributeMap.get(attr)!.max = max;
+    return clone;
+  }
+
+  getRange(attr: string) : Range {
+    return this.attributeMap.get(attr)!;
+  }
+
+}
+
+class Range {
+  min: number; // inclusive
+  max: number; // inclusive
+
+  constructor(min: number, max: number) {
+    this.min = min;
+    this.max = max;
+  }
+
+  clone() {
+    return new Range(this.min, this.max);
+  }
+
+  getWidth() {
+    return (this.max - this.min) + 1
+  }
 }
 
 export enum ComparisonType {
